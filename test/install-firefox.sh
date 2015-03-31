9fe4107c014ddcabdd0fc4fc26abbe5a1dc2a03a
Aurimas Vinckevicius <aurimas.dev@gmail.com>
2015-03-30 20:19:42 -0500

    Use absolute paths for FX_EXECUTABLE

--- test/install-firefox.sh
+++ test/install-firefox.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-sudo rm -f /usr/local/bin/firefox
-
 if [ "$FIREFOXVERSION" = "ESR" ] ; then
   ESR_VERSION="31.5.0esr"
   wget http://ftp.mozilla.org/pub/firefox/releases/${ESR_VERSION}/linux-x86_64/en-US/firefox-${ESR_VERSION}.tar.bz2
   tar -xjf firefox-${ESR_VERSION}.tar.bz2
-  FX_EXECUTABLE="firefox/firefox"
+  FX_EXECUTABLE="`readlink -f firefox/firefox`"
 elif [ "$FIREFOXVERSION" = "stable" ] || [ "$FIREFOXVERSION" = "beta" ] || [ "$FIREFOXVERSION" = "aurora" ] ; then
   if [ "$FIREFOXVERSION" = "beta" ] ; then
     sudo add-apt-repository ppa:mozillateam/firefox-next -y
@@ -15,7 +13,7 @@ elif [ "$FIREFOXVERSION" = "stable" ] || [ "$FIREFOXVERSION" = "beta" ] || [ "$F
   fi
   sudo apt-get update -q
   sudo apt-get install firefox -V --reinstall
-  FX_EXECUTABLE="`which firefox`"
+  FX_EXECUTABLE="/usr/bin/firefox"
 fi
 
 export FX_EXECUTABLE
