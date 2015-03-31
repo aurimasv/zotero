220510b90d58b567ecbc616d2ca450b043364feb
Aurimas Vinckevicius <aurimas.dev@gmail.com>
2015-03-30 19:33:49 -0500

    Use FX_EXECUTABLE

--- test/install-firefox.sh
+++ test/install-firefox.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
 
-if [ "$FIREFOXVERSION" = "stable" ] ; then
-  sudo apt-get update
-  sudo apt-get install firefox
-elif [ "$FIREFOXVERSION" = "ESR" ] ; then
+ps -e
+
+if [ "$FIREFOXVERSION" = "ESR" ] ; then
   ESR_VERSION="31.5.0esr"
   wget http://ftp.mozilla.org/pub/firefox/releases/${ESR_VERSION}/linux-x86_64/en-US/firefox-${ESR_VERSION}.tar.bz2
   tar -xjf firefox-${ESR_VERSION}.tar.bz2
   FX_EXECUTABLE="firefox/firefox"
-elif [ "$FIREFOXVERSION" = "beta" ] ; then
-  sudo add-apt-repository ppa:mozillateam/firefox-next -y
+elif [ "$FIREFOXVERSION" = "stable" ] || [ "$FIREFOXVERSION" = "beta" ] || [ "$FIREFOXVERSION" = "aurora" ] ; then
+  if [ "$FIREFOXVERSION" = "beta" ] ; then
+    sudo add-apt-repository ppa:mozillateam/firefox-next -y
+  elif [ "$FIREFOXVERSION" = "aurora" ] ; then
+    sudo add-apt-repository ppa:ubuntu-mozilla-daily/firefox-aurora -y
+  fi
   sudo apt-get update -q
   sudo apt-get install firefox
-elif [ "$FIREFOXVERSION" = "aurora" ] ; then
-  sudo add-apt-repository ppa:ubuntu-mozilla-daily/firefox-aurora -y
-  sudo apt-get update -q
-  sudo apt-get install firefox
-fi
-
-if [ -z "$FX_EXECUTABLE" ] ; then
   FX_EXECUTABLE="`which firefox`"
 fi
+
+export FX_EXECUTABLE
 
 echo "Installed Firefox  at $FX_EXECUTABLE"
 "$FX_EXECUTABLE" -v
